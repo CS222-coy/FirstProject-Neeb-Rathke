81dbd1e9ee00c308341b92aa922d28052870b774
j-a-neeb <janeeb@bsu.edu>
2022-09-18 16:42:06 -0400

    Added too TODO list

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,5 +1,9 @@
 package edu.bsu.cs222;
 
 public class Main {
-    //TODO make a todo list
+    //TODO write a function to take name of wikipedia article as a string
+    //TODO write a function to quarry wikipedia for the last 30 people to edit based on a input string
+    //TODO write a revision class that contains a name and date
+    //TODO write revisionParser function that takes in JSON that contains 30 or less names and times and returns a list of revision objects
+    //TODO write RevisionFormatter function that takes a revision object and outputs a string formatted to specifications
 }
