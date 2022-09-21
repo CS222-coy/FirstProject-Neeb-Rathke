d467fd86adbc15ebe4c06adfa7e5c49f59f268a3
Chandler Rathke <chandler.rathke@bsu.edu>
2022-09-20 23:21:27 -0400

    Parse Author test passes

--- src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
+++ src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
@@ -11,10 +11,18 @@ import java.io.InputStream;
 public class WikipediaRevisionParserTest {
 
     @Test
-    public void testParse() throws IOException {
+    public void testParseTimestamp() throws IOException {
         WikipediaRevisionParser parser = new WikipediaRevisionParser();
         InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
-        String timestamp = String.valueOf(parser.parse(testDataStream).getTimestamp());
+        String timestamp = String.valueOf(parser.parseTimestamp(testDataStream));
         Assertions.assertEquals("2022-09-17T05:54:56Z", timestamp);
+    }
+
+    @Test
+    public void testParseAuthor() throws IOException {
+        WikipediaRevisionParser parser = new WikipediaRevisionParser();
+        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        String author = String.valueOf(parser.parseAuthor(testDataStream));
+        Assertions.assertEquals("FMSky", author);
     }
 }
