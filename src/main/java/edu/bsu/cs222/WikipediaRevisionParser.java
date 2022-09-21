667382efeb13cfcb0d93df4d80a5a0a2848b9c56
Chandler Rathke <chandler.rathke@bsu.edu>
2022-09-21 00:07:20 -0400

    TestParse passes and the parser class can return all of the revisions

--- src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
@@ -2,10 +2,14 @@ package edu.bsu.cs222;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
 
 public class WikipediaRevisionParser {
+
+    //ArrayList<Revision> listOfRevisions = new ArrayList<>();
 
     //WikipediaRevisionParser now can parse both a timestamp and an author
     public String parseTimestamp(InputStream testDataStream) throws IOException {
@@ -19,4 +23,12 @@ public class WikipediaRevisionParser {
         return author;
     }
 
+    //TODO: WikipediaRevisionParser currently returns a string but needs to return a revision object
+    public String parse(InputStream testDataStream) throws IOException {
+        JSONArray jsonRevisions = JsonPath.read(testDataStream, "$..['user', 'timestamp']");
+
+        //TODO: Need to create for loop to create separate revisions and put them in an arraylist
+
+        return jsonRevisions.toJSONString();
+    }
 }
--- src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
+++ src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
@@ -25,4 +25,15 @@ public class WikipediaRevisionParserTest {
         String author = String.valueOf(parser.parseAuthor(testDataStream));
         Assertions.assertEquals("FMSky", author);
     }
+
+    @Test
+    public void testParse() throws IOException {
+        WikipediaRevisionParser parser = new WikipediaRevisionParser();
+        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        String revisions = String.valueOf(parser.parse(testDataStream));
+        Assertions.assertEquals("[{\"user\":\"FMSky\",\"timestamp\":\"2022-09-17T05:54:56Z\"}," +
+                "{\"user\":\"192.184.216.221\",\"timestamp\":\"2022-09-14T17:02:31Z\"}," +
+                "{\"user\":\"98.246.93.219\",\"timestamp\":\"2022-09-13T21:39:57Z\"}," +
+                "{\"user\":\"75.172.206.240\",\"timestamp\":\"2022-09-13T01:37:51Z\"}]", revisions);
+    }
 }
