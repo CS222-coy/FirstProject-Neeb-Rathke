6ac8a90098ee1b527a23cbe07affa349be039506
gjwitty <gjwitty@bsu.edu>
2022-09-29 13:08:16 -0400

    Fix errors in WikipediaApplication

--- src/main/java/edu/bsu/cs222/WikipediaApplication.java
+++ src/main/java/edu/bsu/cs222/WikipediaApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
