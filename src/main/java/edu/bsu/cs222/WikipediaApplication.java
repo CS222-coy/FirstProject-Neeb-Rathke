5defafec86069abfa763efbd661e0bd56fdf12b5
Chandler Rathke <chandler.rathke@bsu.edu>
2022-10-03 10:47:00 -0400

    Fixed Insets error

--- src/main/java/edu/bsu/cs222/WikipediaApplication.java
+++ src/main/java/edu/bsu/cs222/WikipediaApplication.java
@@ -3,6 +3,7 @@ package edu.bsu.cs222;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
