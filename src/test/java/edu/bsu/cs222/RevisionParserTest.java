7129faf963a3a197c6c1eb30349c27addbd6fbfc
Chandler Rathke <chandler.rathke@bsu.edu>
2022-10-03 18:45:31 -0400

    Created new parser tests

--- src/test/java/edu/bsu/cs222/RevisionParserTest.java
+++ src/test/java/edu/bsu/cs222/RevisionParserTest.java
@@ -19,11 +19,27 @@ public class RevisionParserTest {
     }
 
     @Test
+    public void testParseFourthTimestamp() throws IOException, CustomException {
+        RevisionParser parser = new RevisionParser();
+        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        Revision[] timestamp = (parser.parse(testDataStream));
+        Assertions.assertEquals("2022-09-10T07:27:39Z", timestamp[3].getTimeStamp());
+    }
+
+    @Test
     public void testParseUser() throws IOException, CustomException {
         RevisionParser parser = new RevisionParser();
         InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
         Revision[] user = (parser.parse(testDataStream));
-        Assertions.assertEquals("2022-09-13T02:55:19Z", user[0].getTimeStamp());
+        Assertions.assertEquals("Mitch Ames", user[0].getUser());
+    }
+
+    @Test
+    public void testParseFourthUser() throws IOException, CustomException {
+        RevisionParser parser = new RevisionParser();
+        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        Revision[] user = (parser.parse(testDataStream));
+        Assertions.assertEquals("Collinanderson", user[3].getUser());
     }
 
     @Test
@@ -31,6 +47,5 @@ public class RevisionParserTest {
         InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
         JSONArray titleResult = JsonPath.read(testDataStream,"$..redirects..to");
         Assertions.assertEquals("[\"The Queen\"]", titleResult.toJSONString());
-
     }
 }
